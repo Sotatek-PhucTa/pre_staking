9ae4e33a4acc8e463450da5f533ce8bba5a053c0
phucta <phuc.ta@sotatek.com>
2021-06-23 10:28:32 +0700

    Initialize and do something

---
+++ contracts/StakingRewards.sol
@@ -0,0 +1,4 @@
+pragma solidity>=0.6.11;
+
+import "@pancakeswap/pancake-swap-lib/contracts/math/SafeMath.sol";
+import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/SafeBEP20.sol";
---
+++ contracts/StakingRewardsFactory.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.6.11;
+
+import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/IBEP20.sol";
+import "@pancakeswap/pancake-swap-lib/contracts/access/Ownable.sol";
+
+//import "./StakingRewards.sol";
