81d280d603675d88f867672a968b124760f8717f
Sotatek-PhucTa <phuc.ta@sotatek.com>
2021-07-01 08:49:14 +0700

    Update IStakingRewards

--- contracts/interfaces/IStakingRewards.sol
+++ contracts/interfaces/IStakingRewards.sol
@@ -4,11 +4,11 @@ interface IStakingRewards {
     //Views
     function lastTimeRewardApplicable() external view returns (uint256);
 
-    function rewardPerToken(address rewardToken) external view returns (uint256);
+    function rewardPerToken() external view returns (uint256);
 
-    function earned(address account, address rewardToken) external view returns (uint256);
+    function earned(address account) external view returns (uint256);
 
-    function getRewardForDuration(address rewardToken) external view returns (uint256);
+    function getRewardForDuration() external view returns (uint256);
 
     function totalSupply() external view returns (uint256);
 
@@ -19,7 +19,7 @@ interface IStakingRewards {
 
     function withdraw(uint256 amount) external;
 
-    function getReward(uint256 amount) external;
+    function getReward() external;
 
     function exit() external;
 }
