7cb072dc6c154b73cf5a43be5a9061a2e2181b7f
Sotatek-PhucTa <phuc.ta@sotatek.com>
2021-07-01 08:46:40 +0700

    Rename rewardDistribution to rewardDistributor

--- contracts/RewardsDistributionRecipient.sol
+++ contracts/RewardsDistributionRecipient.sol
@@ -3,10 +3,10 @@ pragma solidity>=0.6.11;
 abstract contract RewardsDistributionRecipient {
     address public rewardDistributor;
 
-    function notifyRewardAmount(address rewardToekn, uint256 reward) external virtual;
+    function notifyRewardAmount(uint256 reward) external virtual;
 
     modifier onlyRewardDitributor() {
-        require(msg.sender == rewardDistributor, "Caller is not RewardDistribution contract");
+        require(msg.sender == rewardDistributor, "Caller is not RewardDistributor contract");
         _;
     }
 }
