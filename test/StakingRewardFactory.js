f5e06eb3109469c024a416eef20acbc1930906da
Sotatek-PhucTa <phuc.ta@sotatek.com>
2021-07-02 14:07:38 +0700

    Add test for StakingRewardFactory::constructor

--- test/StakingRewardFactory.js
+++ test/StakingRewardFactory.js
@@ -2,11 +2,32 @@ const FactoryContract = artifacts.require("StakingRewardsFactory");
 const expect = require('chai').expect;
 
 contract('FactoryContract', (accounts) => {
-    const [creator] = accounts;
-    it("should create contract successfully", async () => {
-        const genesisTime = Date.now() + 10 * 60000;   //Add 10 minutes from now
-        const result = await FactoryContract.new(accounts[1], genesisTime, {from: creator});
-        console.log(result.address);
-        expect(result.address.toString()).to.be.a('string');
+
+    context("#About constructor", async() => {
+        const [creator, simulateRewardToken] = accounts;
+        const timezoneOffset = new Date().getTimezoneOffset();    //in minutes
+        it("should create contract successfully", async () => {
+            const genesisTime = Date.now() + 10 * 60000 + timezoneOffset * 60000;   //Add 10 minutes from now and ourtimezone;
+            const factoryInstance = await FactoryContract.new(simulateRewardToken, genesisTime, {from: creator});
+
+            // Instance should be success
+            expect(factoryInstance).not.equals(undefined);
+
+            const factoryGenesis = await factoryInstance.stakingRewardGenesis();
+            const factoryStakingReward = await factoryInstance.rewardToken();
+
+            // State variables should be correct
+            expect(Number(factoryGenesis)).equals(genesisTime);
+            expect(factoryStakingReward).equals(simulateRewardToken);
+        });
+
+        it("should not create a contract", async() => {
+            const genesisTime = Date.now() - 10 * 60000 + timezoneOffset * 60000;   // Sub 10 minute from now
+            const factoryInstance = await FactoryContract.new(simulateRewardToken, genesisTime, { from: creator});
+            const factoryGenesis = await factoryInstance.stakingRewardGenesis();
+            console.log(Number(factoryGenesis));
+            console.log(factoryInstance.address);
+        })
+        
     });
 });
