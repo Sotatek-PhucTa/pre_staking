ff4be458c38aaf4033e60c740df913817344c67e
Sotatek-PhucTa <phuc.ta@sotatek.com>
2021-07-02 17:49:31 +0700

    Add test for StakingRewardFactory::deploy

--- test/StakingRewardFactory.js
+++ test/StakingRewardFactory.js
@@ -5,10 +5,10 @@ const { time } = require('@openzeppelin/test-helpers');
 
 contract('FactoryContract', (accounts) => {
 
-    context("#About constructor", async() => {
-        const [creator, simulateRewardToken] = accounts;
+    const [creator, simulateRewardToken, simulateStakingToken] = accounts;
+    xcontext("#About constructor", async() => {
         it("should create contract successfully", async () => {
-            const genesisTime = Number(await time.latest()) + 10 * 60000   //Add 10 minutes from now and ourtimezone;
+            const genesisTime = Number(await time.latest()) + 10 * 60 * 000   //Add 10 minutes from now and ourtimezone;
             const factoryInstance = await FactoryContract.new(simulateRewardToken, genesisTime, {from: creator});
 
             // Instance should be success
@@ -29,24 +29,44 @@ contract('FactoryContract', (accounts) => {
         })
     });
 
-    xcontext("#Create a single Farm", async() => {
-        const [creator, simulateRewardToken, simulateStakingToken] = accounts;
-        const timezoneOffset = new Date().getTimezoneOffset() * 60000;
-        let factoryInsance;
+    context("#Create a single Farm", async() => {
+        let factoryInstance;
         const rewardAmount = 600;
         const rewardDuration = 600;
         const vestingPeriod = 1200;
         const splits = 4;
-        const clamable = 20;
-        const deployParams = [simulateStakingToken, rewardAmount, rewardDuration, vestingPeriod, splits, clamable];
+        const claimable = 20;
+        const deployParams = [simulateStakingToken, rewardAmount, rewardDuration, vestingPeriod, splits, claimable];
         beforeEach(async() => {
-            const genesisTime = Date.now() + timezoneOffset * 5 * 1000; // add 5ms from now
+            const genesisTime = Number(await time.latest()) + 10 * 1000  //Add 10 second from now
             factoryInstance = await FactoryContract.new(simulateRewardToken, genesisTime, {from: creator});
         });
 
-        it("should deploy success", async() => {
-            const farmInstance = await factoryInsance.deploy(...deployParams);
+        xit("should deploy success", async() => {
+            const result = await utils.shouldThrow(factoryInstance.deploy(...deployParams, {from: creator}));
+            expect(result).equals(false);
+        });
 
-        }) 
-    })
+        xit("should deploy with correct param", async() => {
+            const result = await factoryInstance.deploy(...deployParams, {from: creator});
+            // Transaction should be success
+            expect(result.receipt.status).equal(true);
+
+            // StakingToken should be added
+            expect(await factoryInstance.stakingTokens(0)).equals(simulateStakingToken);
+
+            const farmIntanceInfo = await factoryInstance.stakingRewardInfosByStakingToken(simulateStakingToken);
+            expect(Number(farmIntanceInfo.rewardAmount)).equals(rewardAmount);
+            expect(Number(farmIntanceInfo.rewardDuration)).equals(rewardDuration);
+            expect(Number(farmIntanceInfo.vestingPeriod)).equals(vestingPeriod);
+            expect(Number(farmIntanceInfo.claimable)).equals(claimable);
+        });
+
+        it("error when deploy contract with same Staking token", async() => {
+            await factoryInstance.deploy(...deployParams, { from: creator });
+            const result = await utils.shouldThrow(factoryInstance.deploy(...deployParams, { from: creator}));
+            expect(result).equals(true);
+        });
+    });
+
 });
