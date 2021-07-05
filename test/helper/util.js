4f71a73fee6f2e771f52ad781cf1ac0aa820c0e9
Sotatek-PhucTa <phuc.ta@sotatek.com>
2021-07-05 15:29:45 +0700

    Set chainid equal to 1

--- test/helper/util.js
+++ test/helper/util.js
@@ -57,7 +57,7 @@ function getDomainSeparator(name, tokenAddress) {
                 ),
                 utils.keccak256(utils.toUtf8Bytes(name)),
                 utils.keccak256(utils.toUtf8Bytes('1')),
-                1337,
+                1,
                 tokenAddress
             ]
         )
